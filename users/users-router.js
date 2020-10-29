330c1d87cdf0d9bdd22addd71466e8adbbd37bc2
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-10-28 17:01:37 -0700

    project completed

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const Users = require("./users-model")
+const bcrypt = require("bcryptjs")
+const {restrict} = require("./users-middleware")
 
 const router = express.Router()
 
-router.get("/users", async (req, res, next) => {
+router.get("/users", restrict(), async (req, res, next) => {
 	try {
 		res.json(await Users.find())
 	} catch(err) {
@@ -24,7 +26,8 @@ router.post("/users", async (req, res, next) => {
 
 		const newUser = await Users.add({
 			username,
-			password,
+			// has the password with a atime complexity of 10
+			password: await bcrypt.hash(password,14),
 		})
 
 		res.status(201).json(newUser)
@@ -38,11 +41,23 @@ router.post("/login", async (req, res, next) => {
 		const { username, password } = req.body
 		const user = await Users.findBy({ username }).first()
 		
+		
 		if (!user) {
 			return res.status(401).json({
 				message: "Invalid Credentials",
 			})
 		}
+		
+		// will be true or false depending on whether the password matched the hash
+		const passwordValid = await bcrypt.compare(password, user.password)
+
+		if (!passwordValid) {
+			return res.status(401).json({
+				message: "Invalid Credentials"
+			})
+		}
+		// creates a new session and sends it back to the client
+		req.session.user = user
 
 		res.json({
 			message: `Welcome ${user.username}!`,
